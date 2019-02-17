9d73c234747816a6d0c21b61507363ef66b08ce8
VenomVendor <info@VenomVendor.com>
2019-02-17 00:02:09 -0500

    Added unit tests for PublicFeed. Added UnitTests for models

---
+++ test-helper/src/test/java/com/venomvendor/tigerspike/testhelper/TestHelper.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2019 VenomVendor. All rights reserved.
+ * Created by VenomVendor on 16-Feb-2019.
+ */
+
+package com.venomvendor.tigerspike.testhelper;
+
+public class TestHelper {
+
+}
